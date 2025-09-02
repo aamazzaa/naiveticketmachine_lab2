e305aa3b1fc5247b534dd93b8909211a3d6d0a08
Anthony <anthonyrobertmazza@gmail.com>
2025-09-02 09:17:52 -0400

    until question 36

--- TicketMachine.java
+++ TicketMachine.java
@@ -61,7 +61,12 @@ public class TicketMachine
     {
         balance = balance + amount;
     }
-
+    
+    public int getTotal()
+    {
+        return total; 
+    }
+    
     /**
      * Print a ticket.
      * Update the total collected and
